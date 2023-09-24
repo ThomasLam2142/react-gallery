3f36b28d83728d61b3a4603dc95ef400a6f44f1e
Thomas Lam <thomaslam2142@gmail.com>
2023-09-24 16:17:04 -0400

    some changes and Added Daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,72 @@ const imageUrls = {
   image1: '/images/image1.jpg',
   image2: '/images/image2.jpg',
   image3: '/images/image3.jpg',
-  // Add more image URLs as needed
+  image4: '/images/image4.jpg',
+  image5: '/images/image5.jpg',
+  image6: '/images/image6.jpg',
+  image7: '/images/image7.jpg',
+  image8: '/images/image8.jpg',
+  image9: '/images/image9.jpg',
+  image10: '/images/image10.jpg',
+  image11: '/images/image11.jpg',
+  image12: '/images/image12.jpg',
+  image13: '/images/image13.jpg',
+  image14: '/images/image14.jpg',
+  image15: '/images/image15.jpg',
+  image16: '/images/image16.jpg',
+  image17: '/images/image17.jpg',
+  image18: '/images/image18.jpg',
+  image19: '/images/image19.jpg',
+  image20: '/images/image20.jpg'
 };
+
+const imgwidth = 600;
+const imgheight = 337.50;
 
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Image Gallery
-      </h1>
+      <span>
+        <h1 className="text-3xl font-bold underline">
+          Image Gallery
+        </h1>
+      </span>
+
+      <div className='searchBar'>
+        <span>
+          <h2>Search for an image:</h2>
+        </span>
+        <input type="text" placeholder="Search for an image..." className="text-lg border rounded-md p-2"/>
+      </div>
+      
       <body>
-        <div>
-          <img src = {imageUrls.image1} className='imgElement' alt="image1" width="700"/>
+        <div className='galleryContainer'>
+
+          <div className='imgContainer'>
+            <img src = {imageUrls.image1} className='imgElement' width={imgwidth} height={imgheight}/>
+          </div>
+          
+          <div className='imgContainer'>
+            <img src = {imageUrls.image2} className='imgElement' width={imgwidth} height={imgheight}/>
+          </div>
+
+          <div className='imgContainer'>
+            <img src = {imageUrls.image3} className='imgElement' width={imgwidth} height={imgheight}/>
+          </div>
+
+          <div className='imgContainer'>
+            <img src = {imageUrls.image4} className='imgElement' width={imgwidth} height={imgheight}/>
+          </div>
+
+          <div className='imgContainer'>
+            <img src = {imageUrls.image5} className='imgElement' width={imgwidth} height={imgheight}/>
+          </div>
+
+          <div className='imgContainer'>
+            <img src = {imageUrls.image6} className='imgElement' width={imgwidth} height={imgheight}/>
+          </div>
+
         </div>
-        
       </body>
         
       
